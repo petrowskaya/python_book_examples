d501399734b5d177375a85ed3d3bdc5af2bcb28c
Elena.Petrovskaya <elena.petrovskaya@ocrv.ru>
2022-03-30 11:06:15 +0300

    add loop.py

---
+++ 01_input_output/05_loop.py
@@ -0,0 +1,13 @@
+a = range(5) # a = 0,1,2,3,4
+b = range(1,8) # b = 1,2,3,4,5,6,7
+c = range(0,14,3) # c = 0,3,6,9,12
+d = range(8,1,-1) # d = 8,7,6,5,4,3,2
+
+for a1 in a : print(a1, end=' ')
+print()
+for b1 in b : print(b1, end=' ')
+print()
+for c1 in c : print(c1, end=' ')
+print()
+for d1 in d : print(d1, end=' ')
+print()
