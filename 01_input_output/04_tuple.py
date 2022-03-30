594e0b0dc57b06cf186e3ef0f135c187cd723058
Elena.Petrovskaya <elena.petrovskaya@ocrv.ru>
2022-03-30 10:45:55 +0300

    add tuple.py

---
+++ 01_input_output/04_tuple.py
@@ -0,0 +1,15 @@
+# Файл содержит строки вида “name,shares,price”
+filename = 'input/portfolio.csv'
+portfolio = []
+for line in open(filename):
+    fields = line.split(';') # Преобразует строку в список
+    name = fields[0] # Извлекает и преобразует отдельные значения полей
+    shares = int(fields[1])
+    price = float(fields[2].replace(',','.'))
+    stock = (name,shares,price) # Создает кортеж (name, shares, price)
+    portfolio.append(stock) # Добавляет в список записей
+
+total = 0.0
+for name, shares, price in portfolio:
+    total += shares * price
+print(total)
