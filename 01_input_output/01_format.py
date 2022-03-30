9e9358334cf73117ade66a82fa59b8e5450ab448
Elena.Petrovskaya <elena.petrovskaya@ocrv.ru>
2022-03-30 10:01:19 +0300

    int2str

--- 01_input_output/01_format.py
+++ 01_input_output/01_format.py
@@ -5,21 +5,31 @@ print ('Format date time: {:02d}-{:02d}-{:04d} {:02d}:{:02d}:{:02d}'.format(now.
 print ('Format date time new: %02d-%02d-%04d %02d:%02d:%02d' % (now.day,now.month,now.year,now.hour,now.minute,now.second))
 
 def min(a):
-    # Ничего не делать
-    pass
+    pass # Ничего не делать
 
 principal = 1000 # Начальная сумма вклада
 rate = 0.05 # Процент
 numyears = 5 # Количество лет
 year = 1
 
-f = open('data/out','w') # Открывает файл для записи
+f = open('output/out','w') # Открывает файл для записи
 while year <= numyears:
     principal = principal * (1 + rate)
-    f.write('%3d %0.2f' % (year,principal))
     print ('%3d %0.2f' % (year,principal))
-    #print ( f,'%3d %0.2f' % (year,principal))
+    #print ('%3d %0.2f' % (year,principal),file=f)
+    f.write('%3d %0.2f\n' % (year,principal))
     year += 1
 f.close()
 
+x = 37
+s1 = 'Значение переменной x: ' + str(x)
+s2 = 'Значение переменной x: ' + repr(x)
+s3 = 'Значение переменной x: ' + format(x,'4d')
+
+print ('%s %s %s' %(s1,s2,s3))
+
+'''
+name = input('Введите свое имя : ')
+print ('%s, добрый день!'% name)
+'''
 
