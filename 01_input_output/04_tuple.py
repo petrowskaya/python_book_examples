750d0682bd86818c4f11eb9dd08a37056ee5b9b1
Elena.Petrovskaya <elena.petrovskaya@ocrv.ru>
2022-03-30 10:48:46 +0300

    add comment

--- 01_input_output/04_tuple.py
+++ 01_input_output/04_tuple.py
@@ -1,3 +1,4 @@
+# Кортежи, списки которые нельзя изменять, предпочтительнее использовать, тк занимают меньше памяти
 # Файл содержит строки вида “name,shares,price”
 filename = 'input/portfolio.csv'
 portfolio = []
@@ -7,7 +8,7 @@ for line in open(filename):
     shares = int(fields[1])
     price = float(fields[2].replace(',','.'))
     stock = (name,shares,price) # Создает кортеж (name, shares, price)
-    portfolio.append(stock) # Добавляет в список записей
+    portfolio.append(stock)     # Добавляет в список записей
 
 total = 0.0
 for name, shares, price in portfolio:
